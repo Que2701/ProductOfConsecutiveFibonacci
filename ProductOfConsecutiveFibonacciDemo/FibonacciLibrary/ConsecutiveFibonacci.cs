77786f96dfc863fbe721b34cd76da592811b1f0d
tumelo <tumelom@evolvemed.co.za>
2020-05-19 20:05:01 +0200

    added solution

---
+++ ProductOfConsecutiveFibonacciDemo/FibonacciLibrary/ConsecutiveFibonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciLibrary
+{
+    public class ConsecutiveFibonacci
+    {
+        public static ulong[] Product(ulong product)
+        {
+            ulong f = 0;
+            ulong fn = 1;
+            while(f * fn < product)
+            {
+                fn = f + fn;
+                f = fn - f;
+            }
+
+            var fx = f * fn == product ? 1 : 0;
+            return new ulong[] { f, fn, (ulong)fx };
+        }
+    }
+}
---
+++ ProductOfConsecutiveFibonacciDemo/ProductOfConsecutiveFibonacciApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using FibonacciLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductOfConsecutiveFibonacciApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in ConsecutiveFibonacci.Product(714))
+            {
+                Console.Write($"{item} ");
+            }
+
+            // Allow the user to close the applicaiton
+            Console.ReadLine();
+        }
+    }
+}
